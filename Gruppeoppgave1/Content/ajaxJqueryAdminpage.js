f2f1a906d125a44a894fb8456619a18229fd5bab
s315614 <s315614@stud.hioa.no>
2018-10-17 22:30:06 +0200

    searchbar, not finished

--- Gruppeoppgave1/Content/ajaxJqueryAdminpage.js
+++ Gruppeoppgave1/Content/ajaxJqueryAdminpage.js
@@ -63,81 +63,52 @@
         });
     })
 
-
+    $("#searchBox").change(function () {
+        var id = $(this).val();
+        $.ajax({
+            url: '/Home/hentFilmInneholder/' + id,
+            type: 'GET',
+            dataType: 'json',
+            success: function (film) {
+                visInfoDynamisk(film);
+            },
+            error: function (x, y, z) {
+                window.location.replace("/Home/AdminPage");
+            }
+        });
+    });
 
 
     function visInfoDynamisk(film) {
 
-        $("#visFilmer").html("");
+        $("#visTabell").html("");
 
-        var htmlRow;
+        var htmlRowTop = '';
 
-        $.each(film, function (i, item) {
+        htmlRowTop += '<table class="table table-striped table-bordered table-hover"><thead class="thead-dark">';
+        htmlRowTop += '<tr><th scope="col">FilmId</th><th scope="col">Navn</th><th scope="col">Beskrivelse</th>';
+        htmlRowTop += '<th scope="col">Pris</th><th scope="col">Kategori</th><th scope="col"></th><th scope="col"></th>';
+        htmlRowTop += '</tr></thead><tbody>';
 
-            var imgSrc = item.BildeTekst;
-            htmlRow = [
 
-                '<a data-toggle="modal" data-target="#' + item.Id + '">',
-                '<div class="col-xs-3 col-center-block">',
-                '<img class="rounded-circle" src="' + imgSrc + '" alt="Generic placeholder image"/>',
-                '<p></p>',
-                '</div>',
-                '</a>',
+        $.each(filmer, function (i, item) {
 
-                '<div class="modal fade" id="' + item.Id + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">',
-                '<div class="modal-dialog" role="document">',
-                '   <div class="modal-content">',
-                '      <div class="modal-header">',
-                '         <h3 class="modal-title" id="exampleModalLabel">' + item.Navn + '</h3>',
-                '        <button type="button" class="close" data-dismiss="modal" aria-label="Close">',
-                '           <span aria-hidden="true">&times;</span>',
-                '      </button>',
-                ' </div>',
-                '<div class="modal-body">',
-                '   <img style="width:100%; height: 270px;" class="rounded-circle" src="' + item.BildeTekst + '" alt="Generic placeholder image">',
-                '  <div>',
-                '     <dl class="dl-horizontal">',
-                '        <dt>',
-                '           Navn',
-                '      </dt>',
-                '     <dd>',
-                item.Navn,
-                '   </dd>',
 
-                '<dt>',
-                '   Beskrivelse',
-                ' </dt>',
-                '<dd>',
-                item.Beskrivelse,
-                '</dd>',
-                '<dt>',
-                '   Pris',
-                '</dt>',
-                '<dd>',
-                item.Pris,
-                '</dd>',
-                '<dt>',
-                '   KategoriId',
-                ' </dt>',
-                '<dd>',
-                item.KategoriNavn,
-                '</dd>',
-                '</dl>',
-                ' </div>',
-                ' </div>',
-                '      <div class="modal-footer">',
-                '           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>',
-                '<button type="button" class="btn btn-primary" onclick="toLogin()">Buy</button>',
-
-                '         </div>',
-                '      </div>',
-                '   </div>',
-                '</div>'
-            ];
-
-            $("#visFilmer").append(htmlRow.join(''));
+            htmlRowTop += '<tr>';
+            htmlRowTop += '<td>' + item.Id + '</td>';
+            htmlRowTop += '<td>' + item.Navn + '</td>';
+            htmlRowTop += '<td>' + item.Beskrivelse + '</td>';
+            htmlRowTop += '<td>' + item.Pris + '</td>';
+            htmlRowTop += '<td>' + item.KategoriNavn + '</td>';
+            htmlRowTop += '<td><button id="update" class="btn btn-primary">Edit</button></td>';
+            htmlRowTop += '<td><button id="update" class="btn btn-danger">Delete</button></td>';
+            htmlRowTop += '</tr>';
 
         });
+
+        htmlRowTop += '</tbody></table>';
+
+        $("#visTabell").append(htmlRowTop);
 
 
     }
